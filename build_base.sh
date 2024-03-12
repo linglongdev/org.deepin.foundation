b87ac0b42823a0086a017575d9b2f967ba860522
wrj97 <wurongjie1@gmail.com>
2024-03-12 16:09:33 +0800

    Create build_base.sh

---
+++ build_base.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Copyright (c) 2022. Uniontech Software Ltd. All rights reserved.
+#
+# Author:     Iceyer <me@iceyer.net>
+#
+# Maintainer: Iceyer <me@iceyer.net>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -x
+
+ARCH=$1
+
+case $ARCH in
+    amd64);;
+    arm64);;
+    "") echo "enter an architecture, like ./checkout_base.sh amd64" && exit;;
+    *) echo "unknow arch \"$ARCH\", supported arch: amd64, arm64" && exit;;
+esac
+
+
+BASE_REF=org.deepin.foundation
+
+# shellcheck source=/dev/null
+source ./package_list.sh
+
+whereis qemu-user-static || sudo apt-get install -y qemu-user-static
+
+sudo rm -rf ${BASE_REF}
+
+sudo mmdebstrap \
+	--components="main,commercial,community" \
+	--variant=minbase \
+	--architectures="$ARCH" \
+	--include=elfutils,file,ca-certificates \
+	beige \
+	${BASE_REF} \
+	https://ci.deepin.com/repo/deepin/deepin-community/stable
+
+
+sudo chown -R "${USER}": ${BASE_REF}
+
